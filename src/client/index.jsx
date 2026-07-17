82eaf6bcb6655d147a6711007c61556e07452d70
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-17 15:39:51 +0100

    Create index.jsx

---
+++ src/client/index.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import GravatarCard from './components/gravatar-card';
+import GravatarSearch from './components/gravatar-search';
+import './style.scss';
+ 
+const App = () => (
+    <div className="gravatar-example">
+        <h1>Hello, Gravatar!</h1>
+        <GravatarSearch />
+        <GravatarCard email="sarahthompson@fork.do" />
+    </div>
+);
+ 
+const container = document.getElementById( 'root' );
+const root = createRoot( container );
+root.render( <App /> );
