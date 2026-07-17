7f17e7bbdcb845de10880bdfc0419c7fe2872a0a
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-17 16:14:18 +0100

    Create example.js

---
+++ etc/example.js
@@ -0,0 +1,14 @@
+const sha256 = require( 'js-sha256' );
+
+function getGravatarURL( email ) {
+  // Trim leading and trailing whitespace from
+  // an email address and force all characters
+  // to lower case
+  const address = String( email ).trim().toLowerCase();
+
+  // Create a SHA256 hash of the final string
+  const hash = sha256( address );
+
+  // Grab the actual image URL
+  return `https://gravatar.com/avatar/${ hash }`;
+}
